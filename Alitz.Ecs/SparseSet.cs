e8f54c4a00f996fe07e80666ddfff038db3fe906
Windawz <46968683+Windawz@users.noreply.github.com>
2023-04-17 00:34:16 +0300

    Make initial draft of sparse set

---
+++ Alitz.Ecs/SparseSet.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alitz.Ecs;
+public class SparseSet<TComponent> where TComponent : struct {
+    private const int NullIndex = -1;
+
+    public SparseSet() {
+        _sparse = new List<int>();
+        _dense = new List<Entity>();
+        _components = new List<TComponent>();
+    }
+
+    private readonly List<TComponent> _components;
+    private readonly List<Entity> _dense;
+    private readonly List<int> _sparse;
+    
+    public IEnumerable<Entity> Entities {
+        get {
+            foreach (Entity entity in _dense) {
+                yield return entity;
+            }
+        }
+    }
+    
+    public IEnumerable<TComponent> Components {
+        get {
+            foreach(TComponent component in _components) {
+                yield return component;
+            }
+        }
+    }
+
+    public TComponent this[Entity entity] {
+        get {
+            (bool contains, _, int entityIndex) = RichContains(entity);
+            if (!contains) {
+                throw new ArgumentOutOfRangeException(nameof(entity));
+            }
+            return _components[entityIndex];
+        }
+        set {
+            (bool contains, bool isInRange, int entityIndex) = RichContains(entity);
+            if (!contains && !isInRange) {
+                ResizeSparseList(entity.Id + 1);
+                entityIndex = _sparse[entity.Id];
+            }
+            if (entityIndex == NullIndex) {
+                entityIndex = _sparse[entity.Id] = _dense.Count;
+                _dense.Add(entity);
+                _components.Add(new TComponent());
+            }
+            _components[entityIndex] = value;
+        }
+    }
+    
+    public bool Contains(Entity entity) =>
+        RichContains(entity).contains;
+    
+    public bool Remove(Entity entity) {
+        (bool contains, _, int entityIndex) = RichContains(entity);
+        if (!contains) {
+            return false;
+        }
+        _sparse[entity.Id] = NullIndex;
+        _dense.RemoveAt(entityIndex);
+        _components.RemoveAt(entityIndex);
+        return true;
+    }
+    
+    private (bool contains, bool isInRange, int entityIndex) RichContains(Entity entity) {
+        bool isInRange = entity.Id < _sparse.Count;
+        int entityIndex = isInRange ? _sparse[entity.Id] : NullIndex;
+        bool contains = isInRange && entityIndex != NullIndex;
+        return (contains, isInRange, entityIndex);
+    }
+
+    private void ResizeSparseList(int count) {
+        if (count < 0) {
+            throw new ArgumentOutOfRangeException(nameof(count));
+        }
+        int currentCount = _sparse.Count;
+        if (count < currentCount) {
+            _sparse.RemoveRange(count, currentCount - count);
+        } else {
+            _sparse.EnsureCapacity(count);
+            _sparse.AddRange(Enumerable.Repeat(NullIndex, count - currentCount));
+        }
+    }
+}
