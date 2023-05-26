75effe9560bb5be96520db1704631cae56326bcd
Windawz <46968683+Windawz@users.noreply.github.com>
2023-05-26 17:24:23 +0300

    Alter RandomInt32DataAttribute to use property initializers for optional arguments

--- Alitz.Common.UnitTests/Utilities/RandomInt32DataAttribute.cs
+++ Alitz.Common.UnitTests/Utilities/RandomInt32DataAttribute.cs
@@ -6,38 +6,36 @@ using Xunit.Sdk;
 
 namespace Alitz.UnitTests.Utilities;
 public class RandomInt32DataAttribute : DataAttribute {
-    public RandomInt32DataAttribute(
-        int? seed = null,
-        int setCount = 1,
-        int minValue = Int32.MinValue,
-        int maxValue = Int32.MaxValue
-    ) {
-        if (setCount < 0) {
-            throw new ArgumentOutOfRangeException(nameof(setCount));
-        }
-        
-        if (seed is not null) {
-            _random = new Random(seed.Value);
-        } else {
-            _random = new Random();
-        }
-        _setCount = setCount;
-        _minValue = minValue;
-        _maxValue = maxValue;
+    public RandomInt32DataAttribute() {
+        Seed = null;
+        SetCount = 1;
+        MinValue = int.MinValue;
+        MaxValue = int.MaxValue;
     }
     
-    private readonly Random _random;
     private readonly int _setCount;
-    private readonly int _minValue;
-    private readonly int _maxValue;
+    
+    public int? Seed { get; init; }
+    public int SetCount {
+        get => _setCount;
+        init {
+            if (value <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+            _setCount = value;
+        } 
+    }
+    public int MinValue { get; init; }
+    public int MaxValue { get; init; }
     
     /// <inheritdoc />
     public override IEnumerable<object[]> GetData(MethodInfo testMethod) {
+        var random = Seed is null ? new Random() : new Random(Seed.Value);
         int parameterCount = testMethod.GetParameters().Length;
-        for (int i = 0; i < _setCount; i++) {
+        for (int i = 0; i < SetCount; i++) {
             var arguments = new object[parameterCount];
             for (int j = 0; j < arguments.Length; j++) {
-                arguments[j] = _random.Next(_minValue, _maxValue);
+                arguments[j] = random.Next(MinValue, MaxValue);
             }
             yield return arguments;
         }
