873302278e7b78e677e298ab6dbb4a0862ac5c9b
Windawz <46968683+Windawz@users.noreply.github.com>
2023-06-06 19:09:52 +0300

    Write new tests for SparseSet

--- Alitz.Ecs.UnitTests/SparseSetTests.cs
+++ Alitz.Ecs.UnitTests/SparseSetTests.cs
@@ -9,4 +9,67 @@ public class SparseSetTests
     }
 
     private readonly SparseSet<int> _set;
+
+    [Fact]
+    public void Contains_TrueForAddedValue()
+    {
+        _set.TryAdd(42);
+        Assert.True(_set.Contains(42));
+    }
+
+    [Fact]
+    public void Contains_FalseForNeverAddedValue() =>
+        Assert.False(_set.Contains(42));
+
+    [Fact]
+    public void Contains_FalseForRemovedValue()
+    {
+        _set.Add(42);
+        _set.Remove(42);
+        Assert.False(_set.Contains(42));
+    }
+
+    [Fact]
+    public void Remove_TrueIfContains()
+    {
+        _set.TryAdd(42);
+        Assert.True(_set.Remove(42));
+    }
+
+    [Fact]
+    public void Remove_FalseIfDoesNotContain() =>
+        Assert.False(_set.Remove(42));
+
+    [Fact]
+    public void Remove_CountDecreases()
+    {
+        _set.Add(42);
+        _set.Add(63);
+        Assert.Equal(2, _set.Count);
+        _set.Remove(42);
+        Assert.Equal(1, _set.Count);
+        _set.Remove(63);
+        Assert.Equal(0, _set.Count);
+    }
+
+    [Fact]
+    public void TryAdd_TrueIfDoesNotContain() =>
+        Assert.True(_set.TryAdd(42));
+
+    [Fact]
+    public void TryAdd_FalseIfContains()
+    {
+        _set.TryAdd(42);
+        Assert.False(_set.TryAdd(42));
+    }
+
+    [Fact]
+    public void TryAdd_CountIncreases()
+    {
+        Assert.Equal(0, _set.Count);
+        _set.TryAdd(42);
+        Assert.Equal(1, _set.Count);
+        _set.TryAdd(63);
+        Assert.Equal(2, _set.Count);
+    }
 }
