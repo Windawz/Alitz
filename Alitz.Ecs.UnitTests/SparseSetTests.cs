28584b6f2397ec27852a9e624d8e9c9564c06953
Windawz <46968683+Windawz@users.noreply.github.com>
2023-06-11 19:08:45 +0300

    Write tests for sparse set regarding correctly handling entity version

--- Alitz.Ecs.UnitTests/SparseSetTests.cs
+++ Alitz.Ecs.UnitTests/SparseSetTests.cs
@@ -7,12 +7,13 @@ public class SparseSetTests
 {
     public SparseSetTests()
     {
-        _entitySpace = new EntitySpace();
+        _space = new EntitySpace();
         _set = new SparseSet<Entity>(IndexExtractor.Entity);
     }
 
-    private readonly EntitySpace _entitySpace;
     private readonly SparseSet<Entity> _set;
+
+    private readonly EntitySpace _space;
 
     [Fact]
     public void EmptyCtor_CreatedEmpty() =>
@@ -21,19 +22,19 @@ public class SparseSetTests
     [Fact]
     public void Contains_TrueForAddedValue()
     {
-        var entity = _entitySpace.Create();
+        var entity = _space.Create();
         _set.TryAdd(entity);
         Assert.True(_set.Contains(entity));
     }
 
     [Fact]
     public void Contains_FalseForNeverAddedValue() =>
-        Assert.False(_set.Contains(_entitySpace.Create()));
+        Assert.False(_set.Contains(_space.Create()));
 
     [Fact]
     public void Contains_FalseForRemovedValue()
     {
-        var entity = _entitySpace.Create();
+        var entity = _space.Create();
         _set.Add(entity);
         _set.Remove(entity);
         Assert.False(_set.Contains(entity));
@@ -42,20 +43,20 @@ public class SparseSetTests
     [Fact]
     public void Remove_TrueIfContains()
     {
-        var entity = _entitySpace.Create();
+        var entity = _space.Create();
         _set.TryAdd(entity);
         Assert.True(_set.Remove(entity));
     }
 
     [Fact]
     public void Remove_FalseIfDoesNotContain() =>
-        Assert.False(_set.Remove(_entitySpace.Create()));
+        Assert.False(_set.Remove(_space.Create()));
 
     [Fact]
     public void Remove_CountDecreases()
     {
-        var entity = _entitySpace.Create();
-        var otherEntity = _entitySpace.Create();
+        var entity = _space.Create();
+        var otherEntity = _space.Create();
         _set.Add(entity);
         _set.Add(otherEntity);
         Assert.Equal(2, _set.Count);
@@ -67,12 +68,12 @@ public class SparseSetTests
 
     [Fact]
     public void TryAdd_TrueIfDoesNotContain() =>
-        Assert.True(_set.TryAdd(_entitySpace.Create()));
+        Assert.True(_set.TryAdd(_space.Create()));
 
     [Fact]
     public void TryAdd_FalseIfContains()
     {
-        var entity = _entitySpace.Create();
+        var entity = _space.Create();
         _set.TryAdd(entity);
         Assert.False(_set.TryAdd(entity));
     }
@@ -80,8 +81,8 @@ public class SparseSetTests
     [Fact]
     public void TryAdd_CountIncreases()
     {
-        var entity = _entitySpace.Create();
-        var otherEntity = _entitySpace.Create();
+        var entity = _space.Create();
+        var otherEntity = _space.Create();
         Assert.Equal(0, _set.Count);
         _set.TryAdd(entity);
         Assert.Equal(1, _set.Count);
@@ -94,7 +95,7 @@ public class SparseSetTests
     {
         foreach (int _ in Enumerable.Range(0, 5))
         {
-            _set.TryAdd(_entitySpace.Create());
+            _set.TryAdd(_space.Create());
         }
         SparseSet<Entity> emptySet = new(IndexExtractor.Entity);
         _set.Clear();
@@ -102,5 +103,35 @@ public class SparseSetTests
             () => Assert.True(
                 _set.Values.OrderBy(entity => entity.Id).SequenceEqual(emptySet.Values.OrderBy(entity => entity.Id))),
             () => Assert.Equal(emptySet.Count, _set.Count));
+    }
+
+    public class EntityVersioningTests
+    {
+        public EntityVersioningTests()
+        {
+            _set = new SparseSet<Entity>(IndexExtractor.Entity);
+            var startEntity = new Entity(Entity.IdMinValue, Entity.VersionMinValue);
+            _set.Add(startEntity);
+        }
+
+        private readonly SparseSet<Entity> _set;
+
+        private Entity StartEntity =>
+            _set.Values.First();
+
+        [Fact]
+        public void TryAdd_TrueIfOnlyVersionDiffers() =>
+            Assert.True(_set.TryAdd(NextVersionOf(StartEntity)));
+
+        [Fact]
+        public void Contains_FalseIfVersionDiffers() =>
+            Assert.False(_set.Contains(NextVersionOf(StartEntity)));
+
+        [Fact]
+        public void Remove_FalseIfVersionDiffers() =>
+            Assert.False(_set.Remove(NextVersionOf(StartEntity)));
+
+        private static Entity NextVersionOf(Entity entity) =>
+            new(entity.Id, entity.Version + 1);
     }
 }
