a9a4a4bba660adc91e058fd5c3c0c5cdd657ca75
Windawz <46968683+Windawz@users.noreply.github.com>
2023-07-17 19:52:45 +0300

    Remove redundant members from IColumn`1 to prevent hiding

--- Src/Alitz.Ecs/Collections/IColumn`1.cs
+++ Src/Alitz.Ecs/Collections/IColumn`1.cs
@@ -3,8 +3,6 @@
 namespace Alitz.Collections;
 public interface IColumn<TComponent> : IColumn where TComponent : struct
 {
-    int Count { get; }
-    IEnumerable<Id> Entities { get; }
     IEnumerable<TComponent> Components { get; }
 
     TComponent this[Id entity] { get; set; }
