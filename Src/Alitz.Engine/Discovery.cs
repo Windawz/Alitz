720d0a17444d441f2f5a0a420e060c427c40783a
Windawz <46968683+Windawz@users.noreply.github.com>
2023-09-04 16:44:02 +0300

    Add method for discovering all system types in assemblies in given directory

---
+++ Src/Alitz.Engine/Discovery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using Alitz.Systems;
+
+namespace Alitz;
+public static class Discovery
+{
+    public static IReadOnlyCollection<Type> DiscoverSystemTypes(string directoryName)
+    {
+        if (!Directory.Exists(directoryName))
+        {
+            throw new DiscoveryException($"Discovery directory {directoryName} does not exist");
+        }
+
+        return Directory.EnumerateFiles(directoryName, "*.dll", SearchOption.TopDirectoryOnly)
+            .Select(
+                fileName =>
+                {
+                    Assembly? maybeAssembly;
+                    try
+                    {
+                        maybeAssembly = Assembly.LoadFile(fileName);
+                    }
+                    catch (Exception exception) when (exception is FileLoadException or BadImageFormatException)
+                    {
+                        maybeAssembly = null;
+                    }
+                    return maybeAssembly;
+                })
+            .Where(maybeAssembly => maybeAssembly is not null)
+            .Cast<Assembly>()
+            .SelectMany(assembly => assembly.ExportedTypes)
+            .Distinct()
+            .Where(type => type.IsAssignableTo(typeof(ISystem)))
+            .ToArray();
+    }
+}
---
+++ Src/Alitz.Engine/DiscoveryException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Alitz;
+public class DiscoveryException : AlitzException
+{
+    public DiscoveryException() { }
+
+    public DiscoveryException(string? message) : base(message) { }
+
+    public DiscoveryException(string? message, Exception? innerException) : base(message, innerException) { }
+}
