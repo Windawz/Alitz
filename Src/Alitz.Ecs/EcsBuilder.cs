54f148e19714e42a0bcd26298c867a185633eeaf
Windawz <46968683+Windawz@users.noreply.github.com>
2023-09-05 09:47:24 +0300

    Rewrite some methods of EcsBuilder and SystemFactory for better usage with runtime-known types

--- Src/Alitz.Ecs/EcsBuilder.cs
+++ Src/Alitz.Ecs/EcsBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
 
 using Alitz.Systems;
 
@@ -10,15 +12,25 @@ public class EcsBuilder
 
     private readonly List<SystemFactory> _factories = new();
 
-    public EcsBuilder AddSystem<TSystem>() where TSystem : class, ISystem, new()
+    public EcsBuilder AddSystem(Type systemType, Func<ISystem>? factory = null)
     {
-        _factories.Add(SystemFactory.Create(() => new TSystem()));
+        bool prevalidateFactoryType = factory is not null;
+        factory ??= MakeFactoryFromParameterlessConstructorOrThrowIfNone(systemType);
+        _factories.Add(new SystemFactory(systemType, factory, prevalidateFactoryType));
         return this;
     }
 
-    public EcsBuilder AddSystem<TSystem>(Func<TSystem> factory) where TSystem : class, ISystem
+    public EcsBuilder AddSystem<TSystem>() where TSystem : class, ISystem, new()
     {
-        _factories.Add(SystemFactory.Create(factory));
+        _factories.Add(new SystemFactory(typeof(TSystem), () => new TSystem(), false));
+        return this;
+    }
+
+    public EcsBuilder AddSystem<TSystem>(Func<TSystem>? factory) where TSystem : class, ISystem
+    {
+        bool prevalidateFactoryType = factory is not null;
+        factory ??= (Func<TSystem>)MakeFactoryFromParameterlessConstructorOrThrowIfNone(typeof(TSystem));
+        _factories.Add(new SystemFactory(typeof(TSystem), factory, prevalidateFactoryType));
         return this;
     }
 
@@ -26,5 +38,21 @@ public class EcsBuilder
     {
         var schedule = new SystemSchedule(_factories);
         return new EntityComponentSystem(schedule);
+    }
+
+    private static Func<ISystem> MakeFactoryFromParameterlessConstructorOrThrowIfNone(Type systemType)
+    {
+        var parameterlessConstructor = systemType.GetConstructor(
+            BindingFlags.Public | BindingFlags.Instance,
+            Array.Empty<Type>());
+
+        if (parameterlessConstructor is null)
+        {
+            throw new ArgumentException(
+                $"System type {systemType} "
+                + "has no public parameterless constructor and no custom factory was provided");
+        }
+
+        return Expression.Lambda<Func<ISystem>>(Expression.New(parameterlessConstructor)).Compile();
     }
 }
--- Src/Alitz.Ecs/Systems/SystemFactory.cs
+++ Src/Alitz.Ecs/Systems/SystemFactory.cs
@@ -3,19 +3,45 @@
 namespace Alitz.Systems;
 internal class SystemFactory
 {
-    private SystemFactory(Func<ISystem> factoryMethod, Type systemType)
+    public SystemFactory(Type systemType, Func<ISystem> factory, bool prevalidateFactoryType = false)
     {
-        _factoryMethod = factoryMethod;
+        if (!systemType.IsAssignableTo(typeof(ISystem)))
+        {
+            throw new ArgumentException(
+                $"Provided system type is not assignable to {nameof(ISystem)}",
+                nameof(systemType));
+        }
         SystemType = systemType;
+        _factory = factory;
+        _prevalidateFactoryType = prevalidateFactoryType;
+        if (_prevalidateFactoryType)
+        {
+            var dummyInstance = _factory();
+            if (!SystemType.IsInstanceOfType(dummyInstance))
+            {
+                throw new ArgumentException(
+                    MakeExceptionMessageAboutFactoryTypeMismatch(SystemType, dummyInstance.GetType()),
+                    nameof(factory));
+            }
+        }
     }
 
-    private readonly Func<ISystem> _factoryMethod;
+    private readonly Func<ISystem> _factory;
+    private readonly bool _prevalidateFactoryType;
 
     public Type SystemType { get; }
 
-    public static SystemFactory Create<TSystem>(Func<TSystem> factoryMethod) where TSystem : class, ISystem =>
-        new(factoryMethod, typeof(TSystem));
+    public ISystem Create()
+    {
+        var instance = _factory();
+        if (!_prevalidateFactoryType && !SystemType.IsInstanceOfType(instance))
+        {
+            throw new InvalidOperationException(
+                MakeExceptionMessageAboutFactoryTypeMismatch(SystemType, instance.GetType()));
+        }
+        return instance;
+    }
 
-    public ISystem Create() =>
-        _factoryMethod();
+    private static string MakeExceptionMessageAboutFactoryTypeMismatch(Type systemType, Type instanceType) =>
+        "Provided factory for system of type " + $"{systemType} produces an instance of a different type ({instanceType})";
 }
