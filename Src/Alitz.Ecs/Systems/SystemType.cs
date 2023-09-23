285cf3897552e5b2e0cb0f35a676c483e07f0222
Windawz <46968683+Windawz@users.noreply.github.com>
2023-09-23 18:40:46 +0300

    Add validator wrapper for system types

---
+++ Src/Alitz.Ecs/Systems/SystemType.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Alitz.Ecs.Systems;
+public class SystemType
+{
+    public SystemType(Type type)
+    {
+        if (!type.IsAssignableTo(typeof(ISystem)))
+        {
+            throw new ArgumentException(paramName: nameof(type), message: $"Type {type} does not implement {typeof(ISystem)}");
+        }
+
+        Type = type;
+    }
+
+    public Type Type { get; }
+
+    public static implicit operator SystemType(Type type) =>
+        new(type);
+}
