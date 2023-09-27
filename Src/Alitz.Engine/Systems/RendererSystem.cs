252517d0637fac4be7e206c28d5c245b3b8ce6ee
Windawz <46968683+Windawz@users.noreply.github.com>
2023-09-27 19:09:11 +0300

    Clear console before rendering

--- Src/Alitz.Engine/Systems/RendererSystem.cs
+++ Src/Alitz.Engine/Systems/RendererSystem.cs
@@ -9,10 +9,13 @@ namespace Alitz.Engine.Systems;
 [RunsAtStage(ReservedStageNumbers.Engine)]
 internal class RendererSystem : ISystem
 {
-    public void Update(ISystemContext context, long deltaMs) => 
+    public void Update(ISystemContext context, long deltaMs)
+    {
+        Console.Clear();
         context.ForEach((Id entity, ref GraphicComponent graphic, ref PositionComponent position) =>
         {
             Console.SetCursorPosition((int)position.X, (int)position.Y);
             Console.Write(graphic.CodePoint);
         });
+    }
 }
