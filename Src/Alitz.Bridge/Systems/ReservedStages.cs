2a8f4f396b19f0ae34f1dc80cdad92b3eedec078
Windawz <46968683+Windawz@users.noreply.github.com>
2023-09-27 18:40:22 +0300

    Add more reserved stages

--- Src/Alitz.Bridge/Systems/ReservedStages.cs
+++ Src/Alitz.Bridge/Systems/ReservedStages.cs
@@ -3,5 +3,7 @@ using Alitz.Ecs.Systems;
 namespace Alitz.Bridge.Systems;
 public static class ReservedStageNumbers
 {
-    public const int Engine = -1;
+    public const int Engine = -2;
+    public const int First = -1;
+    public const int Last = int.MaxValue;
 }
