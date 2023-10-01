be2a31788968806d1275e333aa4bb4f7fc22896b
Windawz <46968683+Windawz@users.noreply.github.com>
2023-10-01 14:00:21 +0300

    Remove forwarding code from SystemType to avoid confusion

--- Src/Alitz.Ecs/Systems/SystemType.cs
+++ Src/Alitz.Ecs/Systems/SystemType.cs
@@ -18,33 +18,6 @@ public class SystemType
     public static implicit operator SystemType(Type type) =>
         new(type);
 
-    public static bool operator==(SystemType left, SystemType right) =>
-        left.Type.Equals(right.Type);
-
-    public static bool operator!=(SystemType left, SystemType right) =>
-        !left.Type.Equals(right.Type);
-
-    public static bool operator==(SystemType left, Type right) =>
-        left.Type.Equals(right);
-
-    public static bool operator!=(SystemType left, Type right) =>
-        !left.Type.Equals(right);
-
-    public static bool operator==(Type left, SystemType right) =>
-        left.Equals(right.Type);
-
-    public static bool operator!=(Type left, SystemType right) =>
-        !left.Equals(right.Type);
-
     public static bool IsValid(Type type) =>
         type.IsAssignableTo(typeof(ISystem));
-
-    public override bool Equals(object? obj) =>
-        Type.Equals(obj);
-
-    public override int GetHashCode() =>
-        Type.GetHashCode();
-
-    public override string ToString() =>
-        Type.ToString();
 }
